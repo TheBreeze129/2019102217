fb841421712bcf780df924ac9b1bc8f2fc44d563
B <wysgene19@khu.ac.kr>
2022-04-08 10:40:34 +0900

    gugudan.py modified to 9dan

--- gugudan.py
+++ gugudan.py
@@ -1,3 +1,5 @@
 for i in range(1,10):
-    for l in range(1,9):
+    print("---------------",i,"단 ---------------\n")
+    for l in range(1,10):
         print("{0} * {1} = {2}".format(i, l, i*l))
+    print('\n')
